498332a09aedc367ae6a9ab70001cb1d217a0978
MJU <marie.ju2012@outlook.com>
2020-02-23 17:47:02 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  def name              # Getter
+    @name
+  end
+  def name=(new_name)   # Setter
+    @name = new_name
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+
+=begin
+myDog = Dog.new("Fido")
+puts(myDog.name)
+myDog.name = "Snoopy"
+puts(myDog.name)
+myDog.breed = "Husky"
+puts(myDog.breed)
+=end
